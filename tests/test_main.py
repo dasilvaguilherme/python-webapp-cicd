d05910575b8e7bc5ba286d7bb9234614bae5a933
Guilherme Silva <guilherme.silva@scaleup.com.br>
2025-01-30 17:30:36 -0300

    fix - import to show app.py file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,10 @@
-from fastapi.testclient import TestClient
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from app import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
