41d514b65f8da6650f60913470bee00c4f29f4aa
Guilherme Silva <guilherme.silva@scaleup.com.br>
2025-01-30 21:58:05 -0300

    adjust workflow

--- test_main.py
+++ test_main.py
@@ -1,8 +1,5 @@
-import os
-import sys
-from fastapi.testclient import TestClient
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from app import app
+from fastapi.testclient import TestClient
 
 client = TestClient(app)
 
