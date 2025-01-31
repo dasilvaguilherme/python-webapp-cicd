6ac6e0cc07d33a96e82987cccb11cf6e82a34fb4
Guilherme Silva <guilherme.silva@scaleup.com.br>
2025-01-30 21:56:02 -0300

    adjust test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,8 @@
 import os
 import sys
 from fastapi.testclient import TestClient
-
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-from app import app 
+from app import app
 
 client = TestClient(app)
 
